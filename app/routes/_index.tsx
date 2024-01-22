3fc27f1b354d37cfb64c0b198c9449fbabc3e75b
Mikhail Nikonov <lounge1k@gmail.com>
2024-01-22 16:01:35 +0000

    fix: file path

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,7 +1,7 @@
 import type { MetaFunction } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 import { Converter } from "~/components/converter";
-import { getCurrency } from "~/server/getCurrency";
+import { getCurrency } from "~/server/get-currency";
 import { CurrencyTable } from "~/components/currency-table";
 import * as styles from "~/styles/global-style.css";
 
